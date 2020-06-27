0c00f17a51017ac483199d7cd59acc0b8b5018d2
Mustafa Siddiqui <mustafa.eins@gmail.com>
2020-06-27 01:56:45 -0400

    Final comments added

--- Pacemaker/main.c
+++ Pacemaker/main.c
@@ -1,30 +1,24 @@
-// 0.Documentation Section 
-// Lab7_HeartBlock, main.c
+/**	@file	main.c
+ * 	@brief	This code works as a simulator for a simple heart pacemaker.
+ * 			It runs on the ARM TM4C123 LaunchPad and was written using the
+ * 			Kiel µVision IDE version 4.74.
+ * 			
+ * 			The input from switch 1 on the launch pad acts as an atrial sensor (AS)
+ * 			on a pacemaker. Output to the green LED on the pad is Ready and is used 
+ * 			for debugging and does not exist on an actual pacemaker. Output to the
+ * 			red LED acts as a ventricular trigger (VT). 
+ * 
+ * 			The program begins by setting Ready as high and waiting for the switch 
+ * 			to be pressed. When it is pressed, it clears Ready (set as low), and 
+ * 			waits for the switch to be released. When it is released, it waits for 
+ * 			250 ms (simulates the time between atrial and ventricular contraction)
+ * 			and sets VT as high which will pulse the ventricles. It then waits for
+ * 			another 250 ms and then clears VT (set as low).
+ * 	@author	Mustafa Siddiqui
+ * 	@date	06/26/20
+ */
 
-// Runs on LM4F120 or TM4C123 LaunchPad
-// Input from PF4(SW1) is AS (atrial sensor), 
-// Output to PF3, Green LED, is Ready,
-// Output to PF1, Red LED, is VT (ventricular trigger) 
-// Make PF4 input, PF3,PF1 output
-// Initialize Ready to high and VT to low
-// Repeat this sequence of operation over and over
-// 1) Wait for AS to fall (touch SW1 switch)
-// 2) Clear Ready low
-// 3) Wait 10ms (debounces the switch)
-// 4) Wait for AS to rise (release SW1)
-// 5) Wait 250ms (simulates the time between atrial and ventricular contraction)
-// 6) set VT high, which will pulse the ventricles 
-// 7) Wait 250ms
-// 8) clear VT low 
-// 9) set Ready high
-
-// Date: January 15, 2016
-
-// 1. Pre-processor Directives Section
-#include "TExaS.h"
-
-// Constant declarations to access port registers using 
-// symbolic names instead of addresses
+/* Define ports */
 #define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
 #define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
 #define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
@@ -35,80 +29,113 @@
 #define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
 #define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
 #define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
-// 2. Declarations Section
-//   Global Variables
 
-//   Function Prototypes
+/**	@fn		void PortF_Init(void)
+ * 	@brief	Initializes Port F on the microcontroller to allow digital function and sets 
+ * 			the inputs (PF4 - SW1) and outputs (PF1 - VT - and PF3 - Ready). 
+ * 			Input is SW1 and Outputs are the LEDs on the launchpad.
+ */
 void PortF_Init(void);
+
+/**	@fn		void Delay1ms(unsigned long)
+ * 	@brief	A function to cause delay for approximately some milliseconds. It assumes an 80 MHz clock.
+ * 	@param	The number of milliseconds to be delayed for.
+ */
 void Delay1ms(unsigned long msec);
-void EnableInterrupts(void);  // Enable interrupts
+
+/**	@fn		void WaitForASLow(void)
+ * 	@brief 	This functions reads the AS input and waits for the signal to be low. If it is
+ * 			low, it returns. If not, it waits and calls itself (recursive function) until it
+ * 			detects the signal to be low.
+ */
 void WaitForASLow(void);
+
+/**	@fn		void WaitForASHigh(void)
+ * 	@brief 	This functions reads the AS input and waits for the signal to be high. If it is
+ * 			high, it returns. If not, it waits and calls itself (recursive function) until it
+ * 			detects the signal to be high.
+ */
 void WaitForASHigh(void);
+
+/**	@fn		void SetVT(void)
+ * 	@brief	This functions sets VT - PF1 - high. It does not affect the other bits in the port.
+ */
 void SetVT(void);
+
+/**	@fn		void ClearVT(void)
+ * 	@brief	This function clears VT - PF1. It does not affect the other bits in the port.
+ */
 void ClearVT(void);
+
+/**	@fn		void SetReady(void)
+ * 	@brief	This functions sets Ready - PF3 - high. It does not affect the other bits in the port.
+ */
 void SetReady(void);
+
+/**	@fn		void ClearReady(void)
+ * 	@brief	This function clears Ready - PF3. It does not affect the other bits in the port.
+ */
 void ClearReady(void);
 
-// 3. Subroutines Section
-// MAIN: Mandatory for a C Program to be executable
+/**	@fn		main()
+ * 	@brief 	Main function of the program which simulates the working of a heart pacemaker.
+ * 			Sets 'Ready' as high initially and then waits for the input (SW1) to go low
+ * 			because of negative logic (switch is being pressed). Delays for 10 ms to allow
+ * 			any bouncing in the switch to be eliminated. It then clears 'Ready' and waits 
+ * 			for the input(SW1) to be high (switch being released). When it goes high, it
+ * 			waits for 250 ms and then sets 'VT' for 250 ms. This process is repeated over.
+ * 	@return	An integer when successfully run.
+ */ 
 int main(void){
-  TExaS_Init(SW_PIN_PF40, LED_PIN_PF31,ScopeOn);  // activate grader and set system clock to 80 MHz
-  PortF_Init();                            // Init port PF4 PF3 PF1    
-  EnableInterrupts();                      // enable interrupts for the grader  
-  while(1){          // Follows the nine steps list above
-    // a) Ready signal goes high
-    // b) wait for switch to be pressed
-    // c) Ready signal goes low
-    // d) wait 10ms
-    // e) wait for switch to be released
-    // f) wait 250ms
-    // g) VT signal goes high
-    // h) wait 250ms
-    // i) VT signal goes low
+
+	// initialize port F
+	PortF_Init();  
+	while(1) {
+
+		// ready signal goes high
 		SetReady();
+
+		// wait for switch to be pressed
 		WaitForASLow();
+
+		// ready signal goes low
 		ClearReady();
+
+		// delay for 10 ms
 		Delay1ms(10);
+
+		// wait for switch to be released
 		WaitForASHigh();
+
+		// delay for 250 ms
 		Delay1ms(250);
+
+		// VT signal goes high
 		SetVT();
 		Delay1ms(250);
+
+		// VT signals goes low
 		ClearVT();
   }
 }
-// Subroutine to initialize port F pins for input and output
-// PF4 is input SW1 and PF3-1 is output LEDs
-// Inputs: None
-// Outputs: None
-// Notes: ...
-void PortF_Init(void){ volatile unsigned long delay;
-  SYSCTL_RCGC2_R |= 0x00000020;      // 1) F clock
-  delay = SYSCTL_RCGC2_R;            // delay to allow clock to stabilize     
-  GPIO_PORTF_AMSEL_R &= 0x00;        // 2) disable analog function
-  GPIO_PORTF_PCTL_R &= 0x00000000;   // 3) GPIO clear bit PCTL  
-  GPIO_PORTF_DIR_R &= ~0x10;         // 4.1) PF4 input,
-  GPIO_PORTF_DIR_R |= 0x0E;          // 4.2) PF3,2,1 output  
-  GPIO_PORTF_AFSEL_R &= 0x00;        // 5) no alternate function
-  GPIO_PORTF_PUR_R |= 0x10;          // 6) enable pullup resistor on PF4       
-  GPIO_PORTF_DEN_R |= 0x1E;          // 7) enable digital pins PF4-PF1
+
+/* Initialize port F */
+void PortF_Init(void) { 
+	volatile unsigned long delay;
+  	SYSCTL_RCGC2_R |= 0x00000020;      // F clock
+  	delay = SYSCTL_RCGC2_R;            // delay to allow clock to stabilize     
+  	GPIO_PORTF_AMSEL_R &= 0x00;        // disable analog function
+  	GPIO_PORTF_PCTL_R &= 0x00000000;   // GPIO clear bit PCTL  
+  	GPIO_PORTF_DIR_R &= ~0x10;         // PF4 input
+  	GPIO_PORTF_DIR_R |= 0x0E;          // PF3,2,1 output  
+  	GPIO_PORTF_AFSEL_R &= 0x00;        // no alternate function
+  	GPIO_PORTF_PUR_R |= 0x10;          // enable pullup resistor on PF4       
+  	GPIO_PORTF_DEN_R |= 0x1E;          // enable digital pins PF4-PF1
 }
-// Color    LED(s) PortF
-// dark     ---    0
-// red      R--    0x02
-// blue     --B    0x04
-// green    -G-    0x08
-// yellow   RG-    0x0A
-// sky blue -GB    0x0C
-// white    RGB    0x0E
 
-
-// Subroutine reads AS input and waits for signal to be low
-// If AS is already low, it returns right away
-// If AS is currently high, it will wait until it to go low
-// Inputs:  None
-// Outputs: None
+/* Wait for AS to be low */
 void WaitForASLow(void){
-// PF4 denotes AS
+	// PF4 denotes AS
 	unsigned long AS = (GPIO_PORTF_DATA_R & 0x10) >> 4;
 	if (AS == 0) {
 		return;
@@ -118,13 +145,9 @@ void WaitForASLow(void){
 	}
 }
 
-// Subroutine reads AS input and waits for signal to be high
-// If AS is already high, it returns right away
-// If AS is currently low, it will wait until it to go high
-// Inputs:  None
-// Outputs: None
+/* Wait for AS to be high */
 void WaitForASHigh(void){
-// PF4 denotes AS
+	// PF4 denotes AS
 	unsigned long AS = (GPIO_PORTF_DATA_R & 0x10) >> 4;
 	if (AS == 1) {
 		return;
@@ -134,49 +157,32 @@ void WaitForASHigh(void){
 	}
 }
 
-// Subroutine sets VT high
-// Inputs:  None
-// Outputs: None
-// Notes:   friendly means it does not affect other bits in the port
+/* Set VT */
 void SetVT(void){
-// PF0 dentoes VT
+	// PF1 dentoes VT
 	GPIO_PORTF_DATA_R |= 0x2;
 }
 
-// Subroutine clears VT low
-// Inputs:  None
-// Outputs: None
-// Notes:   friendly means it does not affect other bits in the port
+/* Clear VT */
 void ClearVT(void){
-// PF0 denotes VT
+	// PF1 denotes VT
 	GPIO_PORTF_DATA_R &= ~0x2;
 }
 
-// Subroutine sets Ready high
-// Inputs:  None
-// Outputs: None
-// Notes:   friendly means it does not affect other bits in the port
+/* Set Ready */
 void SetReady(void){
-// PF3 denotes Ready
+	// PF3 denotes Ready
 	GPIO_PORTF_DATA_R |= 0x8;
 }
 
-
-// Subroutine clears Ready low
-// Inputs:  None
-// Outputs: None
-// Notes:   friendly means it does not affect other bits in the port
+/* Clear Ready */
 void ClearReady(void){
-// PF3 denotes Ready
+	// PF3 denotes Ready
 	GPIO_PORTF_DATA_R &= ~0x8;
 }
 
-// Subroutine to delay in units of milliseconds
-// Inputs:  Number of milliseconds to delay
-// Outputs: None
-// Notes:   assumes 80 MHz clock
+/* Delay function */
 void Delay1ms(unsigned long msec){
-// write this function
 	unsigned long count;
 	while (msec > 0) {
 		count = 14333;
